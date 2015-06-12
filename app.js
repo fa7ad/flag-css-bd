37a2d99fd0588e30ed64027aada89426e06acce9
Fahad Hossain <evilinfinite7@gmail.com>
2015-06-12 18:08:14 +0600

    Sass file + mixin created for the flag experiment using divshot compiler

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-(function($){
-  $.get('app.scss',function(scssData){
-    $.ajax({
-      url: "http://alloy.divshot.com/compile",
-      type: "POST",
-      dataType: "text",
-      data: {
-        compress: true,
-        type: "sass",
-        source: scssData
-      },
-      success: function(css) {
-        $("#cssStyle").text(css);
-      }
-    });
-  });
-})(jQuery);
+(function ( $ ) {
+  $( window ).load( function () {
+    var $scssData = '';
+    $.get( './_flag.scss', function ( $Data ) {
+      $scssData += $Data;
+      $.get( './app.scss', function ( $data ) {
+        $scssData += $data.split( "\n" ).slice( 1 ).join( "\n" );
+        $.ajax( {
+          url: "http://alloy.divshot.com/compile", type: "POST", dataType: "text", data: {
+            compress: true, type: "scss", source: $scssData
+          }, success: function ( css ) {
+            $( 'body' ).append( '<link rel="stylesheet" href="data:text/css;base64,' + btoa( css ) + '"/>' );
+          }
+        } );
+      } );
+    } );
+  } );
+})( jQuery );
